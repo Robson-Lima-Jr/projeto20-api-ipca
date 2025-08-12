8a7eaf4af5ad0acc40d6d0b01db732b5af18e87c
unknown <robsonjuniorsn@gmail.com>
2025-08-12 16:48:26 -0300

    atualização e criação da busca por id

--- index.js
+++ index.js
@@ -5,10 +5,17 @@ import historicoInflacao from './dados/dados.js';
 
 app.get('/historicoIPCA', (req, res) => {
     const ano = parseInt(req.query.ano);
-    
-    const dadosAno = historicoInflacao.filter(item => item.ano === ano);
-    res.json(dadosAno);
+    const resultado = ano ? historicoInflacao.filter(item => item.ano === ano) : historicoInflacao;
+
+    res.json(resultado);
 });
+
+app.get('/historicoIPCA/:id', (req, res) => {
+    const id = parseInt(req.params.id);
+    const resultadoId = historicoInflacao.find(item => item.id === id);
+
+    res.json(resultadoId);
+})
 
 
 app.listen(8080, () => {
