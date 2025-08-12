21938e5554590870478b31c4a422cdeed53f03e7
unknown <robsonjuniorsn@gmail.com>
2025-08-12 16:33:30 -0300

    adição de area de busca via ano

--- index.js
+++ index.js
@@ -3,9 +3,13 @@ const app = express();
 
 import historicoInflacao from './dados/dados.js';
 
-app.get('/', (req, res) => {
-    res.json(historicoInflacao);
+app.get('/historicoIPCA', (req, res) => {
+    const ano = parseInt(req.query.ano);
+    
+    const dadosAno = historicoInflacao.filter(item => item.ano === ano);
+    res.json(dadosAno);
 });
+
 
 app.listen(8080, () => {
     console.log('Servidor rodando na porta 8080');
